9807e2ee39fb4624b97f8ca3b5675026bce7dad0
OrangeChick <retww@naver.com>
2022-10-04 08:45:46 +0900

    docs: Modify dev environment

--- Section02. 1, 2차원 배열 탐색/01. 큰 수 출력하기.js
+++ Section02. 1, 2차원 배열 탐색/01. 큰 수 출력하기.js
@@ -1,13 +1,13 @@
-function solution(arr){         
-  let answer=[];
+function solution(arr) {
+  let answer = [];
 
-  answer.push(arr[0])
-  for(let i = 1; i < arr.length; i++){
-    if(arr[i] > arr[i-1]) answer.push(arr[i]);
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
   }
 
   return answer;
 }
 
-let arr=[7, 3, 9, 5, 6, 12];
+let arr = [7, 3, 9, 5, 6, 12];
 console.log(solution(arr));
